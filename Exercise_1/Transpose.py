f88f32ba6caf7fdb862db3cdaba24618f8d187bd
KrishnenduAS <74754645+KrishnenduAS@users.noreply.github.com>
2021-12-16 21:06:42 +0530

    Create Transpose.py

---
+++ Exercise_1/Transpose.py
@@ -0,0 +1,18 @@
+import numpy as np
+  
+R = int(input("Enter the number of rows of first matrix:"))
+C = int(input("Enter the number of columns of first matrix:"))
+  
+  
+print("Enter the entries  of first matrix in a single line (separated by space): ")
+  
+# User input of entries in a 
+# single line separated by space
+entries = list(map(int, input().split()))
+  
+# For printing the matrix
+matrix1= np.array(entries).reshape(R, C)
+print(matrix1)
+c=np.transpose(matrix1)  
+print("Transpose  of  matrices")
+print(c)
