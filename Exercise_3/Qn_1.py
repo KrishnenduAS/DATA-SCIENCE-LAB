7396fcaf58bf94a62f7954669e0e3a1f9e908b35
KrishnenduAS <74754645+KrishnenduAS@users.noreply.github.com>
2021-12-17 14:44:34 +0530

    Update Qn_1.py

--- Exercise_3/Qn_1.py
+++ Exercise_3/Qn_1.py
@@ -2,5 +2,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 xpoints=np.array([1,2,6,18])
 ypoints=np.array([3,10,12,20])
-plt.plot(xpoints,ypoints)
+plt.plot(xpoints,ypoints,'*:r',mec="g",mfc="g")
 plt.show()
