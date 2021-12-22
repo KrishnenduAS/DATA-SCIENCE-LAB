f469a420392ab216b16a7d15369110957f2398e2
KrishnenduAS <74754645+KrishnenduAS@users.noreply.github.com>
2021-12-22 12:09:32 +0530

    Update Qn_7.py

--- Exercise_3/Qn_7.py
+++ Exercise_3/Qn_7.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 g=['G1','G2','G3','G4','G5']
-m= (22, 30, 35, 35, 26)
-w= (25, 32, 30, 35, 29)
-x=np.arange(len(g))
-plt.bar(x-0.2,m,width=0.4,label='men',color='green')
-plt.bar(x+0.2,m,width=0.4,label='women',color='red')
-plt.xticks(x,g)
+m=[22, 30, 35, 35, 26]
+w=[25, 32, 30, 35, 29]
+X=np.arange(len(g))
+plt.bar(X-0.2,m,width=0.4,label="men",color='green')
+plt.bar(X+0.2,m,width=0.4,label="women",color='red')
+plt.xticks(X,g)
 plt.xlabel('person')
 plt.xlabel('Scores')
 plt.title('Scores by person')
